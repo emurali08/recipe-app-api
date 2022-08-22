62aec7e9a6dbaf1d285260149219f4c361e799a5
emurali08 <emurali08@gmail.com>
2022-08-22 08:35:48 +0530

    Added GitHub Actions.

---
+++ app/app/tests.py
@@ -0,0 +1,13 @@
+"""
+Simple tests
+"""
+
+
+def add(x,y):
+    """Add x and y and return result."""
+    return x+y
+
+
+def subtract(x,y):
+    """Subtract x and y and return result."""
+    return y-x
