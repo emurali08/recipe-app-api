eeb8dce6444885b330d29061723325003af1f896
emurali08 <emurali08@gmail.com>
2022-08-22 09:17:52 +0530

    Added GitHub Actions.

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,13 +1,13 @@
 """
-Simple tests
+Calculator functions
 """
 
 
 def add(x, y):
     """Add x and y and return result."""
-    return x+y
+    return x + y
 
 
 def subtract(x, y):
-    """Subtract x and y and return result."""
-    return y-x
+    """Subtract x from y and return result."""
+    return y - x
